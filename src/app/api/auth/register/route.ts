7ebdbb499eed9c31cedfff18aa39b4f62dd9e521
anri <ro23079k@st.omu.ac.jp>
2026-02-24 13:27:21 +0900

    e

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,11 +1,11 @@
 import { prisma } from "@/lib/prisma"
-import bcrypt from "bcrypt"
+import bcryptjs from "bcryptjs"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   const { attendanceNo, nickname, password } = await req.json()
 
-  const hashed = await bcrypt.hash(password, 10)
+  const hashed = await bcryptjs.hash(password, 10)
 
   const user = await prisma.user.create({
     data: {
